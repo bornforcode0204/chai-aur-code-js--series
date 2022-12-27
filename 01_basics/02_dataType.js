a4de95c7dee0a61040c0fc940b96fd338f801fb9
Shiv Singh <77442797+bornforcode0204@users.noreply.github.com>
2022-12-27 20:31:25 +0000

    adding dataType.js file

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,32 @@
+"use strict" // treat all JS code as a newer version
+
+
+// alert("hello"); // we are using nodejs, not browser
+
+
+
+// tc39.es  // orignal documentation of JS
+
+let name = "Shiv";
+let age = 12;
+let isLoggedIn = false;
+
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null  => standalone value
+// undefined => 
+// symbol => unique
+
+
+//  Object
+// array
+
+
+console.log(typeof "Shiv");
+console.log(typeof 23);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof isLoggedIn);
