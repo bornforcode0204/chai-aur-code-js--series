360b0ce56131ea6e712789665132f58f9645829f
Shiv Singh <77442797+bornforcode0204@users.noreply.github.com>
2023-01-17 21:29:57 +0000

    completing operators

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,30 @@
+let value = 4;
+let negvalue = -value;
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello";
+// let str2 = " Shiv";
+// let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
