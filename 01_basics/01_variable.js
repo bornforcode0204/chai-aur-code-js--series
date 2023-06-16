b22567e27782491725b0b8dbd015988afc9f02e8
Shiv Singh <77442797+bornforcode0204@users.noreply.github.com>
2023-06-16 17:33:49 +0000

    just saving

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -16,3 +16,5 @@ prefer not to use var because of issue in block scope and functional scope
 
 console.log(accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+console.log("Shiv Singh");
